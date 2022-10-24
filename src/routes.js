b3e18d0dd1c611040e1db34cd43f5c65be5319f5
Gleyfeson <gleyfeson.santos@sitiobarreiras.com.br>
2022-10-24 16:49:57 -0300

    NOVO TESTE

--- src/routes.js
+++ src/routes.js
@@ -34,49 +34,49 @@ const Home = React.lazy(() => import("./Views/Home"));
 
 export const routes = [
   {
-    path: "x-_Kz101924i/#/dashboard",
+    path: "/dashboard",
     exact: true,
     name: "Default",
     component: DashboardDefault,
   },
   {
-    path: "x-_Kz101924i/#/basic/button",
+    path: "/basic/button",
     exact: true,
     name: "Basic Button",
     component: UIBasicButton,
   },
   {
-    path: "x-_Kz101924i/#/basic/badges",
+    path: "/basic/badges",
     exact: true,
     name: "Basic Badges",
     component: UIBasicBadges,
   },
   {
-    path: "x-_Kz101924i/#/basic/breadcrumb-paging",
+    path: "/basic/breadcrumb-paging",
     exact: true,
     name: "Basic Breadcrumb Pagination",
     component: UIBasicBreadcrumbPagination,
   },
   {
-    path: "x-_Kz101924i/#/basic/collapse",
+    path: "/basic/collapse",
     exact: true,
     name: "Basic Collapse",
     component: UIBasicCollapse,
   },
   {
-    path: "x-_Kz101924i/#/basic/tabs-pills",
+    path: "/basic/tabs-pills",
     exact: true,
     name: "Basic Tabs & Pills",
     component: UIBasicTabsPills,
   },
   {
-    path: "x-_Kz101924i/#/basic/typography",
+    path: "/basic/typography",
     exact: true,
     name: "Basic Typography",
     component: UIBasicBasicTypography,
   },
   {
-    path: "x-_Kz101924i/#/forms/form-basic",
+    path: "/forms/form-basic",
     exact: true,
     name: "Forms Elements",
     component: FormsElements,
@@ -88,30 +88,30 @@ export const routes = [
     component: BootstrapTable,
   },
   {
-    path: "x-_Kz101924i/#/charts/nvd3",
+    path: "/charts/nvd3",
     exact: true,
     name: "Nvd3 Chart",
     component: Nvd3Chart,
   },
   {
-    path: "x-_Kz101924i/#/maps/google-map",
+    path: "/maps/google-map",
     exact: true,
     name: "Google Map",
     component: GoogleMap,
   },
   {
-    path: "x-_Kz101924i/#/sample-page",
+    path: "/sample-page",
     exact: true,
     name: "Sample Page",
     component: OtherSamplePage,
   },
   {
-    path: "x-_Kz101924i/#/docs",
+    path: "/docs",
     exact: true,
     name: "Documentation",
     component: OtherDocs,
   },
-  { path: "x-_Kz101924i/#/home", exact: true, name: "Home", component: Home },
+  { path: "/home", exact: true, name: "Home", component: Home },
 ];
 
 export default routes;
