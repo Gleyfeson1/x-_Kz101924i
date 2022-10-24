ea8d56a756b53b1d7ce2ad0ee54006cd7321e85f
Gleyfeson <gleyfeson.santos@sitiobarreiras.com.br>
2022-10-24 09:03:44 -0300

    Updates

---
+++ precache-manifest.5de76c2600e2c75f30ffd0f1a684f266.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc4c451f92ca580e937be571ad8c75b",
+    "url": "/ifest/index.html"
+  },
+  {
+    "revision": "eb74f104a98fbbe12486",
+    "url": "/ifest/static/css/1.31a0868c.chunk.css"
+  },
+  {
+    "revision": "8f4fc81836facf321923",
+    "url": "/ifest/static/css/6.da14042e.chunk.css"
+  },
+  {
+    "revision": "2e957146ede388b6b19d",
+    "url": "/ifest/static/css/7.6c6be658.chunk.css"
+  },
+  {
+    "revision": "ef954b502d9ed4bfee10",
+    "url": "/ifest/static/css/9.a8a0b138.chunk.css"
+  },
+  {
+    "revision": "2e1f3530cfe0581c73ba",
+    "url": "/ifest/static/js/0.ea805cea.chunk.js"
+  },
+  {
+    "revision": "eb74f104a98fbbe12486",
+    "url": "/ifest/static/js/1.8950639f.chunk.js"
+  },
+  {
+    "revision": "0c63e45a05da221eed1d",
+    "url": "/ifest/static/js/10.8ee5fa6b.chunk.js"
+  },
+  {
+    "revision": "d51057ff9a676131e17f",
+    "url": "/ifest/static/js/11.514838be.chunk.js"
+  },
+  {
+    "revision": "31555eb3092ec024a13d",
+    "url": "/ifest/static/js/12.15dd8a82.chunk.js"
+  },
+  {
+    "revision": "7c208b0a7b1a22098a4c",
+    "url": "/ifest/static/js/13.e6df9210.chunk.js"
+  },
+  {
+    "revision": "00cf32a72a3b65c30d74",
+    "url": "/ifest/static/js/14.cf61e181.chunk.js"
+  },
+  {
+    "revision": "dba69678f44b821207f2",
+    "url": "/ifest/static/js/15.b98c57a8.chunk.js"
+  },
+  {
+    "revision": "ea398a0f41f22fee0a2c",
+    "url": "/ifest/static/js/16.d607b8ea.chunk.js"
+  },
+  {
+    "revision": "e3419dd5fe8362766632",
+    "url": "/ifest/static/js/17.154d3ea4.chunk.js"
+  },
+  {
+    "revision": "1a9badd8698926913d7b",
+    "url": "/ifest/static/js/18.cad9eca7.chunk.js"
+  },
+  {
+    "revision": "d736c8c95c0c3f856a43",
+    "url": "/ifest/static/js/19.bbeb8438.chunk.js"
+  },
+  {
+    "revision": "12298f96a9c4753fd405",
+    "url": "/ifest/static/js/2.bc2d9636.chunk.js"
+  },
+  {
+    "revision": "72556a0c3b657887d71d",
+    "url": "/ifest/static/js/20.b85690c1.chunk.js"
+  },
+  {
+    "revision": "3e5f444c2e690d549bcf",
+    "url": "/ifest/static/js/21.375c5d57.chunk.js"
+  },
+  {
+    "revision": "23e71239f18e79e478bc",
+    "url": "/ifest/static/js/22.652eadd1.chunk.js"
+  },
+  {
+    "revision": "d3f88636d2e79d7f7460",
+    "url": "/ifest/static/js/23.fd08c76d.chunk.js"
+  },
+  {
+    "revision": "d0ba941c39668fa49458",
+    "url": "/ifest/static/js/24.a6e3a2b5.chunk.js"
+  },
+  {
+    "revision": "670f7e502b85b55c74d2",
+    "url": "/ifest/static/js/25.c6c687f6.chunk.js"
+  },
+  {
+    "revision": "56acd225bc450b119b02",
+    "url": "/ifest/static/js/26.142cd579.chunk.js"
+  },
+  {
+    "revision": "6749cc5c8409ce408d8e",
+    "url": "/ifest/static/js/27.aaeb6202.chunk.js"
+  },
+  {
+    "revision": "3102787457dbace74a0a",
+    "url": "/ifest/static/js/3.f14439ee.chunk.js"
+  },
+  {
+    "revision": "8f4fc81836facf321923",
+    "url": "/ifest/static/js/6.e4cfed81.chunk.js"
+  },
+  {
+    "revision": "cea962807e4807027aba82cd12aecf58",
+    "url": "/ifest/static/js/6.e4cfed81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e957146ede388b6b19d",
+    "url": "/ifest/static/js/7.d5b48a45.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ifest/static/js/7.d5b48a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04d6c61e81d58f80356",
+    "url": "/ifest/static/js/8.d0c054c1.chunk.js"
+  },
+  {
+    "revision": "cff284c596863fde25b7ce27237bb0a5",
+    "url": "/ifest/static/js/8.d0c054c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef954b502d9ed4bfee10",
+    "url": "/ifest/static/js/9.727d575f.chunk.js"
+  },
+  {
+    "revision": "7642bb3f13035f842bdf",
+    "url": "/ifest/static/js/main.b9886659.chunk.js"
+  },
+  {
+    "revision": "1391e8c277cf7e859a82",
+    "url": "/ifest/static/js/runtime-main.9d80016a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ifest/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ifest/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ifest/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ifest/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ifest/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
